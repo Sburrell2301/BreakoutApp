bb327e3e8e53968d7e3e11e229def10be62bf526
Sam Burrell <sburrell@Sams-MacBook-Air-4.local>
2017-03-14 11:17:59 -0500

    Created lose zone

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -21,6 +21,7 @@ class GameScene: SKScene {
         makeBall()
         makePaddle()
         makeBrick()
+        makeLoseZone()
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -85,5 +86,12 @@ class GameScene: SKScene {
         brick.physicsBody?.isDynamic = false
         addChild(brick)
     }
-    
+    func makeLoseZone() {
+        let loseZone = SKSpriteNode(color: UIColor.red, size: CGSize(width: frame.width, height: 50))
+        loseZone.position = CGPoint(x: frame.midX, y: frame.minY + 25)
+        loseZone.name = "loseZone"
+        loseZone.physicsBody = SKPhysicsBody(rectangleOf: loseZone.size)
+        loseZone.physicsBody?.isDynamic = false
+        addChild(loseZone)
+    }
 }
